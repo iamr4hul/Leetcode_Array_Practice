57757602055fa25bab842f8735bc0f501fa55fab
iamr4hul <pvrns12345@gmail.com>
2022-11-12 00:52:52 +0530

    GFG ARRAY Questions practice

--- src/NumberPattern.java
+++ src/NumberPattern.java
@@ -4,7 +4,8 @@ public class NumberPattern {
     public static void main(String[] args) {
         System.out.println(numberPattern(4));
     }
-//       static ArrayList<String> numberPattern(int N) {
+
+    //       static ArrayList<String> numberPattern(int N) {
 //            // code here
 //            ArrayList<String> list = new ArrayList<>();
 //            list.add(0,"1");
@@ -16,32 +17,35 @@ public class NumberPattern {
 //
 //            return list;
 //    }
-//    static ArrayList<String> numberPattern(int N){
-//    // code here
-//    ArrayList<String> arrayList=new ArrayList<>();
-//    String L="1",R="1";
-//    arrayList.add("1");
-//    for (int i=2;i<=N;i++){
-//        arrayList.add(L+i+R);
-//        L = L+i;
-//        R =i+R;
+//    static ArrayList<String> numberPattern(int N) {
+//        // code here
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        String L = "1", R = "1";
+//        arrayList.add("1");
+//        for (int i = 2; i <= N; i++) {
+//            arrayList.add(L + i + R);
+//            L = L + i;
+//            R = i + R;
+//        }
+//        return arrayList;
 //    }
-//    return arrayList;
-//}
     static ArrayList<String> numberPattern(int N){
         // code here
         String s="";
         ArrayList<String> str=new ArrayList<String>();
         for(int i=1; i<=N; i++){
             for(int j=1; j<=i; j++){
-                s=s+String.valueOf(j);
+                s += String.valueOf(j);
             }
             for(int j=i-1; j>0; j--){
-                s=s+String.valueOf(j);
+                s +=String.valueOf(j);
             }
             str.add(s);
-            s="";
+            s = "";
         }
         return str;
     }
+
+
 }
+
--- src/ShuffleArray.java
+++ src/ShuffleArray.java
@@ -4,21 +4,21 @@ public class ShuffleArray {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(shuffle(new int[]{4, 5, 6, 1, 2, 3}, 3)));
     }
-    public static int[] shuffle(int[] nums, int n) {
-
-        int[] answer = new int[nums.length];
-
-        //Index variable for result array.
-        int index=0;
-
-        //start i from first index and j from nth index.
-        for(int i=0,j=n;j<nums.length,j++;i++)
-        {
-            answer[index++] = nums[i];
-            answer[index++] = nums[j];
-        }
-        return answer;
-}
+//    public static int[] shuffle(int[] nums, int n) {
+//
+//        int[] answer = new int[nums.length];
+//
+//        //Index variable for result array.
+//        int index=0;
+//
+//        //start i from first index and j from nth index.
+//        for(int i=0,j=n;j<nums.length,j++;i++)
+//        {
+//            answer[index++] = nums[i];
+//            answer[index++] = nums[j];
+//        }
+//        return answer;
+//}
 
     // 2nd method to do this question with while loop
 
